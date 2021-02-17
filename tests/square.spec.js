f48fa785035c59f79a1474ab4508d7145084c940
Ido Tanne <idovader@gmail.com>
2021-02-16 22:07:26 -0500

    Task: Removed old tests

--- tests/square.spec.js
+++
@@ -1,8 +0,0 @@
-const application = require("../js/square");
-
-describe("Square Function", () => {
-    test("it should take 'a' argument and return the value to the power of 2", () => {
-
-        expect(application(6)).toEqual(36);
-    });
-});
