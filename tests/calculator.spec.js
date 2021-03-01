832ac79967da205b6f6aaa52289c9fe780d9fcfd
Ido Tanne <idovader@gmail.com>
2021-03-01 12:23:53 -0500

    Fix: Changed Filename for test cases

--- tests/calculator.spec.js
+++ tests/calculator.spec.js
@@ -3,7 +3,7 @@ const csvReader = require('../js/csvReader/csvReader');
 let csvFile = new csvReader();
 
 test('Calculator adding two numbers', () => {
-    for(row in csvFile.readCSV('testCases/sumTest.csv')){
+    for(row in csvFile.readCSV('testCases/testSum.csv')){
         /*For row in csvReader*/
         let result = Calculator.sum(row[0],row[1]) /*row[0],row[1]*/
         expect(result).toBe(row[2]); /*toBe(row[2])*/
@@ -11,31 +11,31 @@ test('Calculator adding two numbers', () => {
 
 });
 test('Calculator difference between two numbers', () => {
-    for(row in csvFile.readCSV('testCases/sumTest.csv')) {
+    for(row in csvFile.readCSV('testCases/testSubtraction.csv')) {
         let result = Calculator.subtract(row[0], row[1]) /*row[0],row[1]*/
         expect(result).toBe(row[2]); /*toBe(row[2])*/
     }
 });
 test('Calculator product of two numbers', () => {
-    for(row in csvFile.readCSV('testCases/sumTest.csv')) {
+    for(row in csvFile.readCSV('testCases/testMultiply.csv')) {
         let result = Calculator.multiply(row[0], row[1]) /*row[0],row[1]*/
         expect(result).toBe(row[2]); /*toBe(row[2])*/
     }
 });
 test('Calculator quotient of two numbers', () => {
-    for(row in csvFile.readCSV('testCases/sumTest.csv')) {
+    for(row in csvFile.readCSV('testCases/testDivide.csv')) {
         let result = Calculator.divide(row[0], row[1]) /*row[0],row[1]*/
         expect(result).toBe(row[2]); /*toBe(row[2])*/
     }
 });
 test('Calculator power squared of a number', () => {
-    for(row in csvFile.readCSV('testCases/sumTest.csv')) {
+    for(row in csvFile.readCSV('testCases/testPower.csv')) {
         let result = Calculator.power(row[0], row[1]) /*row[0],row[1]*/
         expect(result).toBe(row[2]); /*toBe(row[2])*/
     }
 });
 test('Calculator squared root of a number', () => {
-    for(row in csvFile.readCSV('testCases/sumTest.csv')) {
+    for(row in csvFile.readCSV('testCases/testRoot.csv')) {
         let result = Calculator.root(row[0], row[1]) /*row[0],row[1]*/
         expect(result).toBe(row[2]); /*toBe(row[2])*/
     }
